70b3fdd7c661a6149f3c3af9ec0c85baca2fbd3a
tejasM17 <tejasm9380@gmail.com>
2026-04-27 16:11:45 +0530

    Delete app directory

--- app/src/main/java/com/tejas/chatai/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.tejas.chatai
-
-object Constants {
-    const val API = "AIzaSyCg8nG1an19uUK8lWygM95BZD9-pv8Kmt8"
-}
--- app/src/main/java/com/tejas/chatai/data/MesssageModel.kt
+++
@@ -1,6 +0,0 @@
-package com.tejas.chatai.data
-
-data class MessageModel(
-    val message: String,
-    val role: String,
-)
